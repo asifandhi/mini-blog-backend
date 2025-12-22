5452b18ece5a325a3f1c2951721f838e26dc9917
asifandhi <asifandhi267@gmail.com>
2025-12-22 09:17:33 +0530

    Implement user authentication methods: add password hashing, token generation, and refresh token handling

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,8 @@
 import mongoose, {Schema} from "mongoose";
 
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
 const userSchema = new Schema(
     {
         username :{
@@ -44,11 +47,59 @@ const userSchema = new Schema(
             type : String,
             required : [true, "Password is required"],
             minLength : 8
+        },
+        refreshTokens : {
+            type : String
         }
     },
     {
         timestamps : true
     }
 )
+
+// this is also middleware
+userSchema.pre("save",async function(next) {
+    // if password is not modified then do not hash it again
+    if(!this.isModified("password")){return next();}
+    this.password = await bcrypt.hash(this.password,10);
+    next()
+})
+
+// for cheking the password
+userSchema.methods.isPasswordIsCorrect = async function (password) {
+    
+    return await bcrypt.compare(password,this.password)
+}
+
+// for generating the Access token
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username : this.username,
+            email : this.email,
+            fullname : this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRES_IN
+        }
+    )
+}
+
+// for generating the Refresh token
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRES_IN
+        }
+    )
+}
 
 export const User = mongoose.model("User",userSchema);
